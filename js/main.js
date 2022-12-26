5d5ecf7765491bc0091dd86f34ff8e9eeb59c1c0
Shirley Azurin <wheeloftheyear159@gmail.com>
2022-12-26 11:28:19 -0500

    Añade función para mostrar nombre

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ function selectPoke() {
 
     document.getElementById('pokemon').addEventListener('change', (e) => {
         showPokemonImg(e.target.value);
+        showPokemonName(e.target.value);
     });
     
 }
@@ -34,10 +35,24 @@ function showPokemonImg(pokemonName) {
     .then((response) => response.json())
     .then((data) => {
         const img = data.sprites.front_default;
-        console.log(img);
+        
         const img_container = document.getElementById('img_container');
         img_container.innerHTML = `<img src="${img}" alt="${pokemonName}">`;
     });
 }
 
+function showPokemonName(pokemonName) {
+    const pokeName = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
 
+    fetch(pokeName, {
+        method: 'get'
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        const name = data.name;
+
+        const text_container = document.getElementById('text_container');
+        text_container.innerHTML = `<h2 class="text-center">${name}</h2>`;
+    })
+    
+}
