448629483a3f55ce74a262b68e4b9d2207ddaf20
Shirley Azurin <wheeloftheyear159@gmail.com>
2022-12-26 10:24:36 -0500

    Actualizando values del select

--- js/main.js
+++ js/main.js
@@ -10,34 +10,12 @@ function selectPoke() {
         let html = '';
         
         for (const i in pokemons) {
-            html += '<option value=' + pokemons[i].id + '>' + pokemons[i].name + '</option>'
+            html += '<option value=' + pokemons[i].name + '>' + pokemons[i].name + '</option>'
         }
 
         document.getElementById('pokemon').innerHTML = html;
-    //     for (const index in pokemons) {
-    //         document.write(pokemons[index].name);
-    //         document.write("<br>");
-    // }
     });
     
 }
 
 selectPoke()
-
-
-// function selectPoke() {
-//     fetch(pokeApi, {
-//         method: 'get'
-//     })
-//     .then((response) => response.json())
-//     .then(function (allpokemon) {
-//         allpokemon.results.forEach(function (pokemon){
-//             fetchPokemonData(pokemon);
-//         })
-//     })
-    
-// }
-
-// function fetchPokemonData(pokemon){
-    
-// }
